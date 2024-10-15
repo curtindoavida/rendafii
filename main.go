3688473a3d11984ba116fdb8580a0e8760fb7cac
Marco Santos <marco@sociap.com.br>
2024-10-15 14:53:15 -0300

    init

--- main.go
+++ main.go
@@ -8,24 +8,26 @@ import (
 )
 
 func main() {
+	fmt.Println(getPrice("SNEL11"))
+	fmt.Println(getPrice("BRCR11"))
+	fmt.Println(getPrice("XPML11"))
+}
+
+func getPrice(ticker string) (float64, float64, float64, float64){
 	hoje := time.Now()
 	antes := hoje.AddDate(-1, 0, 0)
 	depois := hoje.AddDate(0, 1, 0)
-	acao := "BRCR11.SA"
-	//acao := "SNEL11.SA"
-	//acao := "ADA-USD"
-	strAnteOntem := "2024-10-01"
-	strOntem := "2024-11-01"
+	acao := ticker + ".SA"
 
-	fmt.Println(antes.Format("2006-01-02"), depois.Format("2006-01-02"), strOntem, strAnteOntem)
 	spy, _ := quote.NewQuoteFromYahoo(acao, antes.Format("2006-01-02"), depois.Format("2006-01-02"), quote.Daily, true)
 	// fmt.Print(spy.CSV())
-	fmt.Println(len(spy.Close))
-	fmt.Println(spy.Close[len(spy.Close)-1])
+	// fmt.Println(len(spy.Close))
+//	fmt.Println(spy.Close[len(spy.Close)-1])
 	rsi := talib.Rsi(spy.Close, 30)
-	fmt.Println(rsi[len(spy.Close)-1])
+//	fmt.Println(rsi[len(spy.Close)-1])
 	rsi2 := talib.Rsi(spy.Close, 90)
-	fmt.Println(rsi2[len(spy.Close)-1])
+//	fmt.Println(rsi2[len(spy.Close)-1])
 	rsi3 := talib.Rsi(spy.Close, len(spy.Close)-1)
-	fmt.Println(rsi3[len(spy.Close)-1])
+//	fmt.Println(rsi3[len(spy.Close)-1])
+	return spy.Close[len(spy.Close)-1], rsi[len(spy.Close)-1], rsi2[len(spy.Close)-1], rsi3[len(spy.Close)-1]
 }
